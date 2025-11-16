0019dbc270a35202658ed7725b97f7e26ad5aefa
Anshika Chauhan <chauhanshikaa1@gmail.com>
2025-11-17 00:21:54 +0530

    Update and rename 49. Group Anagrams to 49 Group Anagrams.py

---
+++ 49 Group Anagrams.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        anagram_groups = defaultdict(list)
+        
+        for s in strs:
+       
+            key = "".join(sorted(s))
+    
+            anagram_groups[key].append(s)
+            
+       
+        return list(anagram_groups.values())
